8774fea857b764d344809d613f1e8f430b89f61d
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2026-02-01 14:38:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let userInput=parseInt(prompt("Enter the number:"));
+
+function fact(n){
+	if(n==1){
+		return 1
+	}
+	return n*fact(n-1)
+}
+
+
+console.log(alert(`The factorial of [number] is ${fact(userInput)}.`))
+
+
